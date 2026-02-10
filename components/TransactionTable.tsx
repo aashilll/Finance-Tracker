9e2963c118ddc3b9c874ffa6689932361da41b37
aashilll <aashilhussain005@gmail.com>
2026-02-10 16:38:30 +0800

    Remove unnecessary type check in TransactionTable

--- components/TransactionTable.tsx
+++ components/TransactionTable.tsx
@@ -147,7 +147,7 @@ export function TransactionTable({
                     }`}
                   >
                     {tx.type === "INCOME" ? "+" : "-"}
-                    {currencyFormat(typeof tx.amount === 'object' ? parseFloat(tx.amount.toString()) : tx.amount)}
+                    {currencyFormat(tx.amount)}
                   </TableCell>
                   <TableCell className="text-right">
                     <div className="flex justify-end gap-2">
